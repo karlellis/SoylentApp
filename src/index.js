98acfd69cc4191d7c5db54bc7ca28599660dbbe0
karlellis <soylent.tv@gmail.com>
2023-08-08 19:58:50 +0200

    Removed Bootstrap folder. Replaced with web v5.1.3

--- src/index.js
+++ src/index.js
@@ -5887,9 +5887,13 @@ class Main extends React.Component {
           </section>
         </div>
         {/* BOOTSTRAP JS */}
-        <script src="./bootstrap/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          crossorigin="anonymous">
         </script>
+        {/* <script src="./bootstrap/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+        </script> */}
       </body>
 
     );
